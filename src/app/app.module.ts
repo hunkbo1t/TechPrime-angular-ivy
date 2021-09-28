e4753645b1555a05b78376b20488eff59b36d0c5
Daivaanshu Soanii <daivaanshusoanii@gmail.com>
2021-09-28 19:28:43 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatFormFieldModule} from '@angular/material/form-field'; 
-import {MatInputModule} from '@angular/material/input'; 
-import {MatButtonModule} from '@angular/material/button'; 
-import {MatCardModule} from '@angular/material/card'; 
-import {MatToolbarModule} from '@angular/material/toolbar'; 
-const routes : Routes = [
-  {path:'',component:LoginComponent}
-];
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+
+const routes: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, FormsModule,
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes),
-    FlexLayoutModule, MatFormFieldModule,
-    MatInputModule, MatButtonModule,
-    MatCardModule, MatToolbarModule
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpClientModule
   ],
-  declarations: [ AppComponent, HelloComponent, LoginComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, HelloComponent, LoginComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
