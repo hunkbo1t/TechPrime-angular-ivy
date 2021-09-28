6415e6750ce6ed0dbcbde92e7c669953cc26b348
Daivaanshu Soanii <daivaanshusoanii@gmail.com>
2021-09-28 18:28:36 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatCardModule} from '@angular/material/card'; 
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+const routes : Routes = [
+  {path:'',component:LoginComponent}
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule, FormsModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule, MatFormFieldModule,
+    MatInputModule, MatButtonModule,
+    MatCardModule, MatToolbarModule
+  ],
+  declarations: [ AppComponent, HelloComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
